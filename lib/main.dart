45b46b6df39c570be25e93cb5685a756f7252a60
Amir Parsa <develop.apm@gmail.com>
2020-11-20 14:28:53 +0330

    Fix android deprecate issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -17,42 +16,9 @@ void main() {
   );
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 6;
-  int rightDiceNumber = 6;
-
-  void changeDicesFace() {
-    leftDiceNumber = Random().nextInt(6) + 1;
-    rightDiceNumber = Random().nextInt(6) + 1;
-  }
-
-  Widget buildDice(int diceNumber) {
-    return Expanded(
-      child: FlatButton(
-        onPressed: () {
-          setState(() {
-            changeDicesFace();
-          });
-        },
-        child: Image.asset('images/dice$diceNumber.png'),
-      ),
-    );
-  }
-
+class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          buildDice(leftDiceNumber),
-          buildDice(rightDiceNumber),
-        ],
-      ),
-    );
+    return Container();
   }
 }
